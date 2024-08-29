12a6a3bcc8ff26dee70eb7bdb5461017d52a82ed
Kelvin Lee <yckelvin@users.noreply.github.com>
2024-08-29 08:47:19 +0800

    remove clear screen

--- main.ts
+++ main.ts
@@ -27,7 +27,7 @@ locations = [
 "bedroom"
 ]
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
-ESP8266_IoT.connectWifi("ssid", "password")
+ESP8266_IoT.connectWifi("MMLC_AP", "12345678")
 basic.showNumber(1)
 let client_id = randint(0, 99999999)
 ESP8266_IoT.setMQTT(
@@ -37,7 +37,7 @@ convertToText(client_id),
 "test",
 ""
 )
-ESP8266_IoT.connectMQTT("192.168.0.242", 1884, false)
+ESP8266_IoT.connectMQTT(" 10.107.122.40", 1884, false)
 basic.showNumber(2)
 OLED.init(128, 64)
 basic.showNumber(3)
@@ -45,4 +45,3 @@ basic.pause(2000)
 if (ESP8266_IoT.isMqttBrokerConnected()) {
     basic.showIcon(IconNames.Yes)
 }
-basic.clearScreen()
